ebe26fa9ac7f6989bc2a56a5f5848dddf904c452
Apeiria adong660 <adong660@icloud.com>
2023-09-23 21:32:50 +0800

    Added lab 3 codes

---
+++ lab03/MMult_openblas.c
@@ -0,0 +1,9 @@
+#include "cblas.h"
+
+void MY_MMult( int m, int n, int k, double *a, int lda, 
+                                    double *b, int ldb,
+                                    double *c, int ldc ) {
+    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans,
+                m, n, k, 1.0, a, lda, b, ldb, 1.0, c, ldc);
+    return;
+}
