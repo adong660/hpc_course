2d8e85dc141786fccb8e3a19118b86aa6ad91ec9
Apeiria adong660 <adong660@icloud.com>
2023-10-10 21:54:47 +0800

    Correct comments

--- lab07/dgemm_mpi.h
+++ lab07/dgemm_mpi.h
@@ -3,10 +3,10 @@
 
 #define MAIN_PROCESS 0
 
-/* MPI implementation of matrix multiplication */
+/* Naive implementation of matrix multiplication */
 void naive_dgemm(int m, int n, int k, double *a, int lda, double *b, int ldb, double *c, int ldc);
 
-/* Naive implementation of matrix multiplication */
+/* MPI implementation of matrix multiplication */
 void mpi_dgemm(int my_id, int num_processes, int m, int n, int k, 
                double *a, int lda, double *b, int ldb, double *c, int ldc);
 
