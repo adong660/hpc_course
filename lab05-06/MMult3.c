319b9ae1114f50f784d7eeb18295173f93245738
Apeiria adong660 <adong660@icloud.com>
2023-10-11 18:35:23 +0800

    Adjust lab 5 function names

--- lab05-06/MMult3.c
+++ lab05-06/MMult3.c
@@ -6,83 +6,81 @@
 #define B(i,j) b[ (j)*ldb + (i) ]
 #define C(i,j) c[ (j)*ldc + (i) ]
 
-#define BLOCK_SIZE 20
+#define BLOCK_SIZE 32
+
+static void full_block_mult(int k, double *a, int lda, 
+                            double *b, int ldb,
+                            double *c, int ldc );
+
+static void notfull_block_mult(int m, int n, int k, double *a, int lda, 
+                               double *b, int ldb,
+                               double *c, int ldc );
 
 /* Routine for computing C = A * B + C */
 /* (m*n) = (m*k) * (k*n) */
 void MY_MMult( int m, int n, int k, double *a, int lda, 
                                     double *b, int ldb,
                                     double *c, int ldc ) {
-    int i = 0, j = 0, p = 0;
-    for (i = 0; i <= m - BLOCK_SIZE; i += BLOCK_SIZE) {
-        for (j = 0; j <= n - BLOCK_SIZE; j += BLOCK_SIZE) {
-            for (p = 0; p <= k - BLOCK_SIZE; p += BLOCK_SIZE) {
-                for (int jj = j; jj < j + BLOCK_SIZE; jj++) {
-                    for (int pp = p; pp < p + BLOCK_SIZE; pp++) {
-                        for (int ii = i; ii < i + BLOCK_SIZE; ii++) {
-                            C(ii, jj) += A(ii, pp) * B(pp, jj);
-                        }
-                    }
-                }
-            }
-            for (int jj = j; jj < j + BLOCK_SIZE; jj++) {
-                for (int pp = p; pp < k; pp++) {
-                    for (int ii = i; ii < i + BLOCK_SIZE; ii++) {
-                        C(ii, jj) += A(ii, pp) * B(pp, jj);
-                    }
-                }
+    int x, y;       // number of blocks in horizontal and verticle directions
+    x = n / BLOCK_SIZE;
+    y = m / BLOCK_SIZE;
+
+    double *block_a, *block_b, *block_c;        // position of blocks
+
+    for (int i = 0; i < y; i++) {
+        for (int j = 0; j < x; j++) {
+            block_c = &C(i * BLOCK_SIZE, j * BLOCK_SIZE);
+            block_a = &A(i * BLOCK_SIZE, 0);
+            block_b = &B(0, j * BLOCK_SIZE);
+            full_block_mult(k, block_a, lda, block_b, ldb, block_c, ldc);
+        }
+    }
+
+    int right_edge, down_edge;
+    right_edge = x * BLOCK_SIZE;
+    down_edge  = y * BLOCK_SIZE;
+    for (int i = 0; i < y; i++) {
+        block_c = &C(i * BLOCK_SIZE, right_edge);
+        block_a = &A(i * BLOCK_SIZE, 0);
+        block_b = &B(0, right_edge);
+        notfull_block_mult(BLOCK_SIZE, n - right_edge, k, block_a, lda, block_b, ldb,  block_c, ldc);
+    }
+    for (int j = 0; j < x; j++) {
+        block_c = &C(down_edge, j * BLOCK_SIZE);
+        block_a = &A(down_edge, 0);
+        block_b = &B(0, j * BLOCK_SIZE);
+        notfull_block_mult(m - down_edge, BLOCK_SIZE, k, block_a, lda, block_b, ldb,  block_c, ldc);
+    }
+    {
+        block_c = &C(down_edge, right_edge);
+        block_a = &A(down_edge, 0);
+        block_b = &B(0, right_edge);
+        notfull_block_mult(m - down_edge, n - right_edge, k, block_a, lda, block_b, ldb, block_c, ldc);
+    }
+}
+
+static void full_block_mult(int k, double *a, int lda, 
+                            double *b, int ldb,
+                            double *c, int ldc ) {
+    int i, j, p;
+    for (j = 0; j < BLOCK_SIZE; j++) {
+        for (p = 0; p < k; p++) {
+            for (i = 0; i < BLOCK_SIZE; i++) {
+                C(i, j) = C(i, j) + A(i, p) * B(p, j);
             }
         }
     }
-    for (j = 0; j <= n - BLOCK_SIZE; j += BLOCK_SIZE) {
-        for (p = 0; p <= k - BLOCK_SIZE; p += BLOCK_SIZE) {
-            for (int jj = j; jj < j + BLOCK_SIZE; jj++) {
-                for (int pp = p; pp < p + BLOCK_SIZE; pp++) {
-                    for (int ii = i; ii < m; ii++) {
-                        C(ii, jj) += A(ii, pp) * B(pp, jj);
-                    }
-                }
-            }
-        }
-        for (int jj = j; jj < j + BLOCK_SIZE; jj++) {
-            for (int pp = p; pp < k; pp++) {
-                for (int ii = i; ii < m; ii++) {
-                    C(ii, jj) += A(ii, pp) * B(pp, jj);
-                }
-            }
-        }
-    }
-    for (i = 0; i <= m - BLOCK_SIZE; i += BLOCK_SIZE) {
-        for (p = 0; p <= k - BLOCK_SIZE; p += BLOCK_SIZE) {
-            for (int jj = j; jj < n; jj++) {
-                for (int pp = p; pp < p + BLOCK_SIZE; pp++) {
-                    for (int ii = i; ii < i + BLOCK_SIZE; ii++) {
-                        C(ii, jj) += A(ii, pp) * B(pp, jj);
-                    }
-                }
-            }
-        }
-        for (int jj = j; jj < n; jj++) {
-            for (int pp = p; pp < k; pp++) {
-                for (int ii = i; ii < i + BLOCK_SIZE; ii++) {
-                    C(ii, jj) += A(ii, pp) * B(pp, jj);
-                }
-            }
-        }
-    }
-    for (p = 0; p <= k - BLOCK_SIZE; p += BLOCK_SIZE) {
-        for (int jj = j; jj < n; jj++) {
-            for (int pp = p; pp < p + BLOCK_SIZE; pp++) {
-                for (int ii = i; ii < m; ii++) {
-                    C(ii, jj) += A(ii, pp) * B(pp, jj);
-                }
-            }
-        }
-    }
-    for (int jj = j; jj < n; jj++) {
-        for (int pp = p; pp < k; pp++) {
-            for (int ii = i; ii < m; ii++) {
-                    C(ii, jj) += A(ii, pp) * B(pp, jj);
+}
+
+static void notfull_block_mult(int m, int n, int k,
+                              double *a, int lda, 
+                              double *b, int ldb,
+                              double *c, int ldc ) {
+    int i, j, p;
+    for (j = 0; j < n; j++) {
+        for (p = 0; p < k; p++) {
+            for (i = 0; i < m; i++) {
+                C(i, j) = C(i, j) + A(i, p) * B(p, j);
             }
         }
     }
